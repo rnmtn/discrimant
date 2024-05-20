ded07e08d3fd5b1adda4c4defbac8df3b53412a4
rnmtn <150450132+rnmtn@users.noreply.github.com>
2024-05-20 04:54:14 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Discriminant Calculator</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+<div class="container">
+    <h2>Calculating b^2 - 4ac </h2>
+    <form method="post" action="result.php">
+        <label for="a">a:</label>
+        <input type="number" id="a" name="a" required><br>
+        <label for="b">b:</label>
+        <input type="number" id="b" name="b" required><br>
+        <label for="c">c:</label>
+        <input type="number" id="c" name="c" required><br>
+        <button type="submit">Calculate</button>
+    </form>
+</div>
+
+</body>
+</html>
